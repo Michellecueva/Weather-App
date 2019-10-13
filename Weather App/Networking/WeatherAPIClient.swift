1bc937958e235e1ab096946eecea99142135b5ba
Michelle <Michelle.cueva@gmail.com>
2019-10-13 17:46:34 -0400

    Creates Api Helper

--- Weather App/Networking/WeatherAPIClient.swift
+++ Weather App/Networking/WeatherAPIClient.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+struct WeatherAPIClient {
+    
+    
+    static let manager = WeatherAPIClient()
+    
+
+    
+    func  getWeatherURL(txtfieldText: String) -> URL {
+     guard let url = URL(string: "") else {fatalError("Error: Invalid URL")}
+     return url
+}
+    
+
+    func getWeather(userInput: String, completionHandler: @escaping (Result<[Weather], AppError>) -> Void) {
+        NetworkHelper.manager.performDataTask(withUrl: getWeatherURL(txtfieldText: userInput), andMethod: .get) { result in
+            switch result {
+            case let .failure(error):
+                completionHandler(.failure(error))
+                return
+            case let .success(data):
+                do {
+                    let weather = try WeatherWrapper.decodeElementsFromData(from: data)
+                    completionHandler(.success(weather))
+                }
+                catch {
+                    completionHandler(.failure(.couldNotParseJSON(rawError: error)))
+                }
+            }
+        }
+    }
+
+    private init() {}
+}
