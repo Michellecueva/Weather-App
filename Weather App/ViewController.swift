5d81aafbd89b491d6fa5924acfcd75333caccae6
Michelle <Michelle.cueva@gmail.com>
2019-10-13 09:56:45 -0400

    Creates file

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Michelle Cueva on 10/13/19.
+//  Copyright © 2019 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
